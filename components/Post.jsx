c49ebd2aa69801f4bfae22a46646c877b4efe0b4
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-11 02:58:39 +0330

    BlogPage added

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import CategoryLabel from './CategoryLabel';
 
 const Post = ({ post }) => {
   return (
@@ -17,7 +18,7 @@ const Post = ({ post }) => {
         <span className="font-light text-gray-600">
           {post.frontMatter.date}
         </span>
-        <div>{post.frontMatter.category}</div>
+        <CategoryLabel>{post.frontMatter.category}</CategoryLabel>
       </div>
 
       <div className="mt-2">
