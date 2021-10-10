de8ac3e309360235d4fc440cf86ccee34ff58172
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-11 02:43:09 +0330

    HomePage update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
 import React from 'react';
 import Layout from '../components/Layout';
+import Link from 'next/link';
+import Post from '../components/Post';
 
-const HomePage = () => {
+const HomePage = ({ posts }) => {
+  console.log(posts);
   return (
     <Layout>
-      <div>Hello World</div>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post}/>
+        ))}
+      </div>
+
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 };
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    const { data: frontMatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontMatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
