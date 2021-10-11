3aa96fcf12bfeac430080107fde934754fa8b924
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-11 11:52:58 +0330

    category page added

--- components/CategoryLabel.jsx
+++ components/CategoryLabel.jsx
@@ -14,7 +14,7 @@ const CategoryLabel = ({ children }) => {
     <div
       className={`px-2 py-1 bg-${colorKey[children]}-600 text-gray-100 font-bold rounded`}
     >
-      <Link href={`blog/category/${children.toLowerCase()}`}>{children}</Link>
+      <Link href={`/blog/category/${children.toLowerCase()}`}>{children}</Link>
     </div>
   );
 };
---
+++ pages/blog/category/[category_name].jsx
@@ -0,0 +1,80 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+import { sortByData } from '@/utils/index';
+
+import React from 'react';
+import Layout from '@/components/Layout';
+import Link from 'next/link';
+import Post from '@/components/Post';
+
+const HomePage = ({ posts, categoryName }) => {
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">
+        Posts in {categoryName}
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'));
+  const categories = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    const { data: frontMatter } = matter(markdownWithMeta);
+
+    return frontMatter.category.toLowerCase();
+  });
+
+  const paths = categories.map((category) => ({
+    params: { category_name: category },
+  }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { category_name } }) {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    const { data: frontMatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontMatter,
+    };
+  });
+
+  // Filter Posts by category
+  const categoryPosts = posts.filter(
+    (post) => post.frontMatter.category.toLowerCase() === category_name
+  );
+
+  return {
+    props: {
+      posts: categoryPosts.sort(sortByData),
+      categoryName: category_name,
+    },
+  };
+}
